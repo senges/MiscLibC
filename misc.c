e52559563d1713ecd5f6882aed3dd3f09b9785bc
Charles SENGES <charles.senges.etu@univ-lille.fr>
2020-09-27 15:34:37 +0200

    idk but push before erase

--- misc.c
+++ misc.c
@@ -4,6 +4,30 @@
 
 #include "misc.h"
 
+/* Array pretty print */
+void pprint(void * buffer, size_t size, type_t t) {
+    int i;
+    char format[STANDARD_BUFFER_SIZE];
+
+    switch (t)
+    {
+    case INT:
+        snprintf(format, 5, "[%d] ");
+        break;
+    case FLOAT:
+        snprintf(format, 7, "[%.2f] ");
+        break;
+    case STRING:
+        snprintf(format, 5, "[%s] ");
+        break;
+    default:
+        printf("Unknown type\n");
+        return;
+    }
+
+    /* Not yet implemented */
+}
+
 /* Print on stdout if v_flag != 0 */
 int verbose(const char * restrict format, ...) {
     if(v_flag == 0)
--- misc.h
+++ misc.h
@@ -1,3 +1,5 @@
+#define STANDARD_BUFFER_SIZE    255
+
 #define DEFAULT_ERR_CODE    -1
 #define DEFAULT_ERR_MSG     "Unknown error"
 
@@ -6,5 +8,13 @@
 #define panic(M,E)  program_panic(M, E)
 
 unsigned char v_flag;   /* Verbose flag */
+
+enum type_e {
+    INT,
+    FLOAT,
+    STRING
+};
+
+typedef enum type_e type_t;
 
 int verbose(const char * restrict format, ...);
